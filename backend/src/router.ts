29a706e3f828596fe43657307e50571f57b22c8a
ArthurRodrigomarques <ArthurRodrigomarques@users.noreply.github.com>
2024-11-21 19:11:53 -0300

    user controller
    crud dos usuários

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,3 +1,15 @@
 import { Router } from "express";
+import {
+  createUser,
+  deleteUser,
+  getAllUser,
+  getUniqueUser,
+} from "./controller/userController";
 
 export const router = Router();
+
+// Rotas de usuário
+router.post("/register", createUser);
+router.delete("/delete-user", deleteUser);
+router.get("/get-all-users", getAllUser);
+router.get("/get-unique-user/:id", getUniqueUser);

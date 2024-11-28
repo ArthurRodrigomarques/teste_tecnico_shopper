4801fca61675730599fc966d7019c2af10611ebe
ArthurRodrigomarques <ArthurRodrigomarques@users.noreply.github.com>
2024-11-27 22:12:03 -0300

    add
    add

--- frontend/src/components/DriverDetails.tsx
+++ frontend/src/components/DriverDetails.tsx
@@ -18,7 +18,7 @@ const DriverDetails: React.FC<DriverDetailsProps> = ({ driver, onGoBack }) => {
   return (
     <div className="mt-5">
       <h3 className="text-2xl font-semibold">Detalhes do Motorista Selecionado:</h3>
-      <div className="border p-4 mt-4 rounded-lg bg-gray-100">
+      <div className="border p-4 mt-4 rounded-lg">
         <h4 className="text-xl font-bold">{driver.name}</h4>
         <p><strong>Carro:</strong> {driver.car}</p>
         <p><strong>Avaliação:</strong> {driver.rating}</p>
